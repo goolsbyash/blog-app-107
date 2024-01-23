2f86de08b4758b30c6f91ff5d76aa943e1e767ae
goolsbyash <goolsbyashley015@gmail.com>
2024-01-23 14:24:01 -0500

    Started adding styled components.

---
+++ src/StyleComponents/styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 80%;
+  margin: auto;
+  height: 80%;
+`;
+
+export const HeaderStyle = styled.h1`
+  color: red;
+`;
+
+export const BlogTitle = styled.h2`
+  color: black;
+`;
+
+export const BlogLink = styled.a`
+  color: red;
+  text-decoration: none;
+  appearance: button;
+`;
+
+export const BlogFooter = styled.ul`
+  list-style-type: none;
+`;
+
+export const StyleFooter = styled.footer`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+export const Continue = styled.a`
+  color: red;
+`;
