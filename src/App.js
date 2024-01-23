fbcac72b40da592432182b6ed68388dd2dc54c5e
goolsbyash <goolsbyashley015@gmail.com>
2024-01-23 11:18:07 -0500

    Created foundation of React app.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="fashion-bog-app">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
